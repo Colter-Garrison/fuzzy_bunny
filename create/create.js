b9b60a012a586cee48e2b64d59e9eeaf41fc49c8
Colter Garrison <102547626+Colter-Garrison@users.noreply.github.com>
2022-05-23 17:10:48 -0700

    everything working

--- create/create.js
+++ create/create.js
@@ -12,8 +12,8 @@ form.addEventListener('submit', async (e) => {
     e.preventDefault();
     const bunnyForm = new FormData(form);
     await createBunny({
-        name: bunnyForm.get('name'),
-        family_id: bunnyForm.get('family_id'),
+        name: bunnyForm.get('bunny-name'),
+        family_id: bunnyForm.get('family-id'),
     });
     form.reset();
     window.location.href = '/families';
@@ -28,18 +28,6 @@ window.addEventListener('load', async () => {
         option.textContent = family.name;
         familyId.append(option);
     }
-    // let's dynamically fill in the families dropdown from supabase
-    // grab the select HTML element from the DOM
-
-    // go get the families from supabase
-
-    // for each family
-
-    // create an option tag
-
-    // set the option's value and text content
-
-    // and append the option to the select
 });
 
 
